bf7eec357e1dfa601b2ca05ed5c9552464772264
Brett Hughes <twofivinclimber@gmail.com>
2022-04-19 13:42:17 -0500

    removed js to try other soloution

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 
 const avail = document.getElementsByClassName("availability");
 const soldOut = document.getElementById("sold-out");
-
-for (let i = 0; i < avail.length; i++){
-if (avail[i].text === "Available") {
-  soldOut.style.display = " ";
-}else{
-  soldOut.style.display = "none";
-}
-};
-
-
-console.log(avail);
