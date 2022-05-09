94180ec9bf98bddbfcef8b1425bce81edea24a1f
Brett Hughes <twofivinclimber@gmail.com>
2022-05-09 11:44:12 -0500

    modularized and am deleting hard HTML

---
+++ data.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    id: 1,
+    name: "Dynamic Rope",
+    image: "images/Rope.jpeg",
+    description:
+      "This rope is great sport climbing. It features a 9.8mm diameter and is good for wet and dry conditions. It's been noted that it has a smoothe handle, and slightly stiff catch.",
+    availability: "Available",
+    size: "70 Meter",
+    weight: "9.5 Lbs",
+    price: "$200",
+    remainingStock: 0,
+  },
+  {
+    id: 2,
+    name: "Climbing Shoes",
+    image: "images/climbing-shoe.jpeg",
+    description:
+      "Good stiff climbing shoes for jamming into cracks and hanging on small edges.  Comfortable out of the box and will last you a long time",
+    availability: "Available",
+    size: "Mens 5-13",
+    weight: "1.2 lbs",
+    price: "$98",
+    remainingStock: 12,
+  },
+  {
+    id: 3,
+    name: "Chalk",
+    image: "images/chalk.webp",
+    description:
+      "halk for you hands, chalk for you knees, chalk for you to let everyone know that you're a climber.  It keep your fingers dry.  That should be enough.",
+    availability: "Available",
+    size: "4in x 4in x 1.5in",
+    weight: "4oz",
+    price: "$3.75",
+    remainingStock: 15,
+  },
+  {
+    id: 4,
+    name: "Rip Stop Pants",
+    image: "images/pants.jpeg",
+    description:
+      "Climbing pants are important when your rubbing all over the rocks.  Keep your knees from bleeding in these rip-stop pants and never give up.",
+    availability: "Available",
+    size: "XS, S, M, L, XL",
+    weight: "12.8oz",
+    price: "$98",
+    remainingStock: 12,
+  },
+  {
+    id: 5,
+    name: "Climbing Harness",
+    image: "images/harness.jpeg",
+    description:
+      "Stay off the ground and out of danger in the air when you fall off your project.  This harness is comftorable for all day sitting and looks cool too.",
+    availability: "Available",
+    size: "XS, S, M, L, XL",
+    weight: "8.2oz",
+    price: "$68",
+    remainingStock: 12,
+  },
+  {
+    id: 6,
+    name: "Camming Device",
+    image: "images/cams.jpeg",
+    description:
+      "For the trad dad in all of us. Take the fear of falling out of your mind knowing you have this device in below you.",
+    availability: "Available",
+    size: ".1 to 6",
+    weight: "6oz - 32oz",
+    price: "$78 - $120",
+    remainingStock: 1,
+  },
+];
+
+export { products };
